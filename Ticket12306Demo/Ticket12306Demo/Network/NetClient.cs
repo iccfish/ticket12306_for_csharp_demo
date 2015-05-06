1d34ed9e6fa6f6b33b8a85e8e017f58c364a2626
木鱼(iFish) <iccfish@qq.com>
2015-05-06 21:00:54 +0800

    建立项目，添加Nuget扩展包及相关文件

---
+++ Ticket12306Demo/Ticket12306Demo/Network/NetClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticket12306Demo.Network
+{
+	using FSLib.Network.Http;
+
+	class NetClient : HttpClient
+	{
+		public NetClient()
+		{
+			
+		}
+	}
+}
---
+++ Ticket12306Demo/Ticket12306Demo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ticket12306Demo
+{
+	using Ticket12306Demo.UI.Dialogs;
+
+	static class Program
+	{
+		/// <summary>
+		/// 应用程序的主入口点。
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+	}
+}
